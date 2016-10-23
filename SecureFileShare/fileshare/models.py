1d7a2b77da1b9fa6f487d77a7f85fc5e7eef5312
ntnayda <ntn4jg@virginia.edu>
2016-10-23 11:38:04 -0400

    added user object

--- SecureFileShare/fileshare/models.py
+++ SecureFileShare/fileshare/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    userName = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
