97f7997163aa5371a6492928c91e07a132c51d39
Brandt <bew5te@virginia.edu>
2016-11-04 15:17:27 -0400

    Added Profile model to be associated with built-in Django User model

--- SecureFileShare/fileshare/admin.py
+++ SecureFileShare/fileshare/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import *
 
+
+class ProfileInLine(admin.StackedInline):
+	model = Profile
+	can_delete = False
+	#verbose_name_plural = 'profile'
+
+class UserAdmin(BaseUserAdmin):
+	inlines = (ProfileInLine, )
+
 # Register your models here.
-admin.site.register(User)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Report)
