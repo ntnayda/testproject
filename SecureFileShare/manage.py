59c59792c2329ddabd2dfb6e910e86873ce4432a
Simonne <sat2uh@virginia.edu>
2016-12-04 23:27:06 -0500

    settings and manage.py fixed

--- SecureFileShare/manage.py
+++ SecureFileShare/manage.py
@@ -8,15 +8,3 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-
-DATABASES = {
-    'default': {
-       	'ENGINE': 'django.db.backends.sqlite3', #but we are no longer using sqlite
-       	'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
- 		}
-}
-
-if os.environ.get('DATABASE_URL'):
-	import dj_database_url
-	db_from_env = dj_database_url.config(conn_max_age=500)
-	DATABASES['default'].update(db_from_env)
